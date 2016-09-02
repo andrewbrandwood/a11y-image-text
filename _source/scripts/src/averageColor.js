ca938af60bdf958df13c2427a9aff42acf47a762
andrewbrandwood <andrew@brandwood.com>
2016-09-02 21:11:20 +0100

    feat: added draggable textarea to codebase

--- _source/scripts/src/averageColor.js
+++ _source/scripts/src/averageColor.js
@@ -12,25 +12,25 @@ function getAverageColourAsRGB (img, textArea) {
 
   // set the height and width of the canvas element to that of the image
   var height = canvas.height = img.naturalHeight || img.offsetHeight || img.height,
-    width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
+      width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
 
   context.drawImage(img, 0, 0);
 
   try {
-  data = context.getImageData(textArea.x, textArea.y, textArea.w, textArea.h);
+    data = context.getImageData(textArea.x, textArea.y, textArea.w, textArea.h);
   } catch(e) {
   // catch errors - usually due to cross domain security issues
-  alert(e);
-  return rgb;
+    alert(e);
+    return rgb;
   }
 
   data = data.data;
   length = data.length;
   while ((i += pixelInterval * 4) < length) {
-  count++;
-  rgb.r += data[i];
-  rgb.g += data[i+1];
-  rgb.b += data[i+2];
+    count++;
+    rgb.r += data[i];
+    rgb.g += data[i+1];
+    rgb.b += data[i+2];
   }
 
   // floor the average values to give correct rgb values (ie: round number values)
@@ -72,13 +72,3 @@ function getAcessibility(textColor, imageColour){
   console.log('WCAG 2 AAA Compliant ', ratio >= 7 ? 'YES' : 'NO');
   console.log('WCAG 2 AAA Compliant (18pt+) ', ratio >= 4.5 ? 'YES' : 'NO');
 }
-
-
-  //
-  //
-  // ratio = Math.round(ratio * 100) / 100; // round to 2 decimal places
-  // document.getElementById('contrastratio').value = ratio;
-  // document.getElementById('w2b').value = (ratio >= 4.5 ? 'YES' : 'NO');
-  // document.getElementById('w2a').value = (ratio >= 3 ? 'YES' : 'NO');
-  // document.getElementById('w2aaab').value = (ratio >= 7 ? 'YES' : 'NO');
-  // document.getElementById('w2aaaa').value = (ratio >= 4.5 ? 'YES' : 'NO');
